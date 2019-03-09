e41e9a9dd532930dde982ede63e741cb3136d221
Rabel Obispo <rabelobispo@hotmail.com>
2019-03-09 17:16:30 -0400

    Creating files in mongodb

--- src/mongodb.js
+++ src/mongodb.js
@@ -17,8 +17,61 @@ MongoClient.connect(connectionUrl, mongoOptions, (error, client) => {
 
   const db = client.db(dbName);
 
-  db.collection('user').insertOne({
-    name: 'Rabel',
-    age: 29
-  });
+  //   db.collection('user').insertOne(
+  //     {
+  //       name: 'Rabel',
+  //       age: 29
+  //     },
+  //     (error, result) => {
+  //       if (error) {
+  //         console.log(error);
+  //         return;
+  //       }
+  //       console.log(result);
+  //     }
+  //   );
+
+  //   db.collection('user').insertMany(
+  //     [
+  //       {
+  //         name: 'Waldo',
+  //         age: 28
+  //       },
+  //       {
+  //         name: 'Titi',
+  //         age: 25
+  //       }
+  //     ],
+  //     (error, result) => {
+  //       if (error) {
+  //         console.log(error);
+  //         return;
+  //       }
+  //       console.log(result.ops);
+  //     }
+  //   );
+
+//   db.collection('task').insertMany(
+//     [
+//       {
+//         description: 'Dentista',
+//         completed: true
+//       },
+//       {
+//         description: 'Mecanico',
+//         completed: false
+//       },
+//       {
+//         description: 'Ir al super',
+//         completed: false
+//       }
+//     ],
+//     (e, r) => {
+//       if (e) {
+//         console.log(e);
+//         return;
+//       }
+//       console.log(r.ops);
+//     }
+//   );
 });
