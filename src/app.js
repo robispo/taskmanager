2e02d39486e45b7a839615714ad4f29396a586f0
Rabel Obispo <rabelobispo@hotmail.com>
2019-03-12 16:21:34 -0400

    Refactoring with async/await

--- src/app.js
+++ src/app.js
@@ -8,56 +8,79 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/api/users', (req, res) => {
+app.post('/api/users', async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(d => res.status(201).json(d))
-    .catch(e => res.status(400).json(e));
+
+  try {
+    await user.save();
+    res.status(201).json(user);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+
+  // user
+  //   .save()
+  //   .then(d => res.status(201).json(d))
+  //   .catch(e => res.status(400).json(e));
 });
 
-app.get('/api/users', (req, res) => {
-  User.find({})
-    .then(d => res.status(200).json(d))
-    .catch(e => res.status(500).json());
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (e) {
+    res.status(500).json();
+  }
 });
 
-app.get('/api/users/:id', (req, res) => {
-  User.findById(req.params.id)
-    .then(d => {
-      if (!d) {
-        res.status(404).json();
-        return;
-      }
-      res.status(200).json(d);
-    })
-    .catch(e => res.status(400).json(e));
+app.get('/api/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      res.status(404).json();
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
-app.post('/api/tasks', (req, res) => {
+app.post('/api/tasks', async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then(d => res.status(201).json(d))
-    .catch(e => res.status(400).json(e));
+
+  try {
+    await task.save();
+    res.status(201).json(task);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
-app.get('/api/tasks', (req, res) => {
-  Task.find({})
-    .then(d => res.status(200).json(d))
-    .catch(e => res.status(500).json());
+app.get('/api/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json(tasks);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
 
-app.get('/api/tasks/:id', (req, res) => {
-  Task.findById(req.params.id)
-    .then(d => {
-      if (!d) {
-        res.status(404).json();
-        return;
-      }
-      res.status(200).json(d);
-    })
-    .catch(e => res.status(400).json(e));
+app.get('/api/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      res.status(404).json();
+      return;
+    }
+
+    res.status(200).json(task);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
 app.listen(port, () => console.log(`Server is up in port ${port}!`));
