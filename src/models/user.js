3591aa84f02d27e59132c71bf37efd084250b7c5
Rabel Obispo <rabelobispo@hotmail.com>
2019-03-13 22:19:41 -0400

    Hashing the password using middleware bcrypt npm model

--- src/models/user.js
+++ src/models/user.js
@@ -1,43 +1,56 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User', {
-    name: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 7,
-      validate(value) {
-        if (value.toLowerCase().includes('password')) {
-          throw new Error('The password can not includes the world "password"!');
-        }
-      }
-    },
-    age: {
-      type: Number,
-      default: 0,
-      validate(value) {
-        if (value < 0) {
-          throw new Error('Age must be a positive number!');
-        }
-      }
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error('Email is invalid!');
-        }
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('The password can not includes the world "password"!');
       }
     }
-  });
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number!');
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid!');
+      }
+    }
+  }
+});
 
-  module.exports = User;
+userSchema.pre('save', async function(next) {
+  const user = this;  
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,88 +4,92 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/api/users', async (req, res) => {
-    const user = new User(req.body);
-  
-    try {
-      await user.save();
-      res.status(201).json(user);
-    } catch (e) {
-      res.status(400).json(e);
-    }
-  
-    // user
-    //   .save()
-    //   .then(d => res.status(201).json(d))
-    //   .catch(e => res.status(400).json(e));
-  });
-  
-  router.get('/api/users', async (req, res) => {
-    try {
-      const users = await User.find({});
-      res.status(200).json(users);
-    } catch (e) {
-      res.status(500).json();
-    }
-  });
-  
-  router.get('/api/users/:id', async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-  
-      if (!user) {
-        res.status(404).json();
-        return;
-      }
-  
-      res.status(200).json(user);
-    } catch (e) {
-      res.status(400).json(e);
-    }
-  });
-  
-  router.patch('/api/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['name', 'email', 'password', 'age'];
-    const isValidOperation = updates.every(p => allowedUpdates.includes(p));
-  
-    if (!isValidOperation) {
-      res
-        .status(400)
-        .json({ error: 'Trying to update a property that is not allowed!' });
+  const user = new User(req.body);
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+
+  // user
+  //   .save()
+  //   .then(d => res.status(201).json(d))
+  //   .catch(e => res.status(400).json(e));
+});
+
+router.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (e) {
+    res.status(500).json();
+  }
+});
+
+router.get('/api/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      res.status(404).json();
       return;
     }
-  
-    try {
-      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-      });
-  
-      if (!user) {
-        res.status(404).json();
-        return;
-      }
-  
-      res.status(200).json(user);
-    } catch (e) {
-      res.status(400).json(e);
-    }
-  });
-  
-  router.delete('/api/users/:id', async (req, res) => {
-    try {
-      const user = await User.findByIdAndDelete(req.params.id);
-  
-      if (!user) {
-        res.status(404).json();
-        return;
-      }
-  
-      res.status(200).json(user);
-    } catch (e) {
-      res.status(400).json(e);
-    }
-  });
 
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+router.patch('/api/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every(p => allowedUpdates.includes(p));
+
+  if (!isValidOperation) {
+    res
+      .status(400)
+      .json({ error: 'Trying to update a property that is not allowed!' });
+    return;
+  }
+
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      res.status(404).json();
+      return;
+    }
+
+    updates.forEach(p => (user[p] = req.body[p]));
+    const userUpdated = await user.save(); 
+
+    res.status(200).json(userUpdated);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+
+     // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true
+    // });
+});
+
+router.delete('/api/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      res.status(404).json();
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
 
 module.exports = router;
